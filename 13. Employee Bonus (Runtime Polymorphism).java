c58326fb40a1177e27ec562e9fd672841da9f00c
Arnav Khapekar <khapekararnav4@gmail.com>
2026-03-25 19:10:25 +0530

    Create 13. Employee Bonus (Runtime Polymorphism).java

---
+++ 13. Employee Bonus (Runtime Polymorphism).java
@@ -0,0 +1,46 @@
+abstract class Employee {
+    String name;
+    int id;
+
+    Employee(String n, int i) {
+        name = n;
+        id = i;
+    }
+
+    abstract double calculateBonus();
+}
+
+class Manager extends Employee {
+    double salary;
+
+    Manager(String n, int i, double s) {
+        super(n, i);
+        salary = s;
+    }
+
+    double calculateBonus() {
+        return 0.2 * salary;
+    }
+}
+
+class Developer extends Employee {
+    double salary, incentive;
+
+    Developer(String n, int i, double s, double inc) {
+        super(n, i);
+        salary = s;
+        incentive = inc;
+    }
+
+    double calculateBonus() {
+        return 0.1 * salary + incentive;
+    }
+
+    public static void main(String[] args) {
+        Employee e1 = new Manager("A", 1, 50000);
+        Employee e2 = new Developer("B", 2, 40000, 5000);
+
+        System.out.println(e1.calculateBonus());
+        System.out.println(e2.calculateBonus());
+    }
+}
