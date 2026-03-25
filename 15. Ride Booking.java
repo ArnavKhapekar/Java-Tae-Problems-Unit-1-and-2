39a6c41e6ddc2cedd4b3f48bb4e507eb92907119
Arnav Khapekar <khapekararnav4@gmail.com>
2026-03-25 19:11:08 +0530

    Create 15. Ride Booking.java

---
+++ 15. Ride Booking.java
@@ -0,0 +1,51 @@
+abstract class Ride {
+    double distance;
+
+    Ride(double d) {
+        distance = d;
+    }
+
+    abstract double calculateFare();
+}
+
+class BikeRide extends Ride {
+    BikeRide(double d) {
+        super(d);
+    }
+
+    double calculateFare() {
+        return distance * 5;
+    }
+}
+
+class AutoRide extends Ride {
+    AutoRide(double d) {
+        super(d);
+    }
+
+    double calculateFare() {
+        return distance * 8;
+    }
+}
+
+class CarRide extends Ride {
+    CarRide(double d) {
+        super(d);
+    }
+
+    double calculateFare() {
+        return distance * 12;
+    }
+
+    public static void main(String[] args) {
+        Ride[] r = {
+            new BikeRide(10),
+            new AutoRide(10),
+            new CarRide(10)
+        };
+
+        for (Ride x : r) {
+            System.out.println(x.calculateFare());
+        }
+    }
+}
