89ff87fd82ef51545427f4054b56965718588731
Arnav Khapekar <khapekararnav4@gmail.com>
2026-03-27 19:59:44 +0530

    Update and rename 19. OnlineShoppingSystem.java to 19. Book Array.java

---
+++ 19. Book Array.java
@@ -0,0 +1,21 @@
+class Book {
+    String title, author;
+    double price;
+
+    Book(String t, String a, double p) {
+        title = t;
+        author = a;
+        price = p;
+    }
+
+    public static void main(String[] args) {
+        Book[] b = {
+            new Book("Java", "James", 500),
+            new Book("Python", "Guido", 600)
+        };
+
+        for (Book x : b) {
+            System.out.println(x.title + " " + x.author + " " + x.price);
+        }
+    }
+}
